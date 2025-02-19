7bfcec631d5d5f56ce259e223baf891298227238
Favio Barberis <faviobarberis@MacBook-Air-de-Favio.local>
2025-02-19 13:22:19 -0300

    fix to deploy

--- pages/api/promotions.js
+++ pages/api/promotions.js
@@ -1,4 +1,4 @@
-import { getSheetData } from '../../lib/sheets';
+import { getSheetData } from '../../lib/sheets.js';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
